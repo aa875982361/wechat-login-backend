0faea2c5b9970bfca5e98bb06768967df9909a62
huangpeilin <huangpeilin@qunjielong.com>
2023-04-18 01:08:00 +0800

    feat: 增加协议

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 //   key: fs.readFileSync('/path/to/domain.key')
 // }
 // const  axiosConfig = { proxy: { host: "127.0.0.1", port: 7890 } }
-const httpsAgent = new httpsProxyAgent({ host: "127.0.0.1", port: 7890 });
+const httpsAgent = new httpsProxyAgent({ host: "http://127.0.0.1", port: 7890 });
 
 // 微信公众号 AppID 和 AppSecret, 需要在微信公众号平台上注册并获取
 const APP_ID = config.APP_ID;
